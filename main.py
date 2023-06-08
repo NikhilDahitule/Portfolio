000071986749a7e5d064f69fe330f85fe34c773d
Nikhil Dahitule <nikhildahitule@gmail.com>
2023-06-08 16:31:11 +0530

    updated

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#main file for branch portfolio_with_mongodb_connectivity
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, send_file
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb+srv://nikhildahitule:RD04%40nd12%23@cluster0.4vw4y5c.mongodb.net/')
+db = client['Portfolio']
+collection = db['Portfolio']
 
 
 @app.route('/')
@@ -15,13 +18,32 @@ def download_resume():
         return send_file('static/resume.txt', as_attachment=True)
     except FileNotFoundError:
         return "File not found"
-# @app.route('/contact',methods=['POST'])
 
 
-# def SubmitContact():
-#   data = request.form
-#   add_application(id, data)
-#   return render_template('application_submitted.html', apply=data, job=job)
+@app.route('/contact', methods=['POST'])
+def submit_contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    mobile_no = request.form.get('mobile')
+    message = request.form.get('message')
+
+    # Store the form data in MongoDB
+    try:
+        store_data_in_mongodb(name, email, mobile_no, message)
+        return render_template('application_submitted.html', name=name)
+    except Exception as e:
+        error_message = f"An error occurred while storing data in MongoDB: {str(e)}"
+        return render_template('error.html', error_message=error_message)
+
+
+def store_data_in_mongodb(name, email, mobile_no, message):
+    data = {
+        'name': name,
+        'email': email,
+        'mobile_no': mobile_no,
+        'message': message
+    }
+    collection.insert_one(data)
 
 
 if __name__ == '__main__':
